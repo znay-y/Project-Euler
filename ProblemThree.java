fab6ced153bd5b201b85f6956faf1bcb7eb8ee5a
Z Laptop <zaineirshad.07@gmail.com>
2026-03-25 08:49:51 +0000

    Extra useless lines removed

--- ProblemThree.java
+++ ProblemThree.java
@@ -2,10 +2,5 @@ import java.math.BigInteger;
 
 public class ProblemThree {
     public static void main(String[] args) {
-        BigInteger largeNumber = new BigInteger("600851475143");
-
-        
-        
-        
     }
 }
